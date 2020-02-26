11135f4cb67e0e79b440fa15ecc5115314ffe5c3
Admin <alan.escalera@fundacion-jala.org>
2020-02-26 15:49:34 -0400

    update test

---
+++ src/main/java/Trabajopolis/MyAccountPage.java
@@ -0,0 +1,31 @@
+package Trabajopolis;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class MyAccountPage extends BasePage{
+
+    @FindBy(linkText = "Buscar Empleos")
+    WebElement buscarEmpleosLink;
+
+    @FindBy(xpath = "/html/body/div[4]/div[6]/div[2]/div/h1")
+    WebElement miCuentaDeCandidatoTxt;
+
+    @FindBy(linkText = "Mi Perfil")
+    WebElement miPerfilLink;
+
+    public MyAccountPage(){}
+
+    @Override
+    protected void waitUntilPageObjectIsLoaded() {
+        webDriverWait.until(ExpectedConditions.visibilityOf(miPerfilLink));
+    }
+    public void clickBuscarEmpleos(){
+        buscarEmpleosLink.click();
+    }
+
+    public String getMiCuentaCantidato(){
+        return miPerfilLink.getText();
+    }
+}

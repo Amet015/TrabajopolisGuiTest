fb485f02f052cc27d635af6099ac3f66386f738b
Admin <alan.escalera@fundacion-jala.org>
2020-02-27 19:38:22 -0400

    test edit salary

--- src/main/java/Trabajopolis/PageTransporter.java
+++ src/main/java/Trabajopolis/PageTransporter.java
@@ -4,13 +4,16 @@ import Core.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
 public class PageTransporter {
-    protected WebDriver webDriver;
+    protected WebDriver webDriver = WebDriverManager.getInstance().getWebDriver();;
     private static final String BASE_URL = "https://www.trabajopolis.bo/";
     // Encargada de leer el url base
     // almacenar como constantes las variaciones
 
     public void navigateToMyAccountPage() {
-        webDriver = WebDriverManager.getInstance().getWebDriver();
         webDriver.get(BASE_URL + "my-account/");
+    }
+
+    public void navigateEditProfilePage() {
+        webDriver.get(BASE_URL + "edit-profile/");
     }
 }

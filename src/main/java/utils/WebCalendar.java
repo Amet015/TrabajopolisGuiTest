e74e9ebed9ec74f42749ab750b2df8193745caf6
Admin <alan.escalera@fundacion-jala.org>
2020-02-28 09:41:35 -0400

    add cucumber and gherkin

--- src/main/java/utils/WebCalendar.java
+++ src/main/java/utils/WebCalendar.java
@@ -8,10 +8,11 @@ import org.openqa.selenium.support.ui.Select;
 
 public class WebCalendar {
     private static Select selectValue;
+    private static int monthInt;
 
     public static void setDateCalendar(String date, WebElement year, WebElement month) {
         String[] dateSplit = date.split("/");
-        int monthInt = Integer.valueOf(dateSplit[0]) - 1;
+        monthInt = Integer.valueOf(dateSplit[0]) - 1;
         selectValue = new Select(year);
         selectValue.selectByValue(dateSplit[2]);
         selectValue = new Select(month);
@@ -22,9 +23,8 @@ public class WebCalendar {
     }
 
     private static WebElement selectDay(String[] dateSplit) {
-        int month = Integer.valueOf(dateSplit[0]) - 1;
         String day = String.format("//td[contains(@data-month,'%s') and contains(@data-year,'%s')]//a[text()='%s']",
-                month, dateSplit[2], dateSplit[1]);
+                monthInt, dateSplit[2], dateSplit[1]);
         return WebDriverManager.getInstance().getWebDriver().findElement(By.xpath(day));
     }
 }
